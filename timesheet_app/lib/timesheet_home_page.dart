e874636bcf70c5981a9f27f37be4a26c03352196
Jason Sanchez <13209003+j4ysanchez@users.noreply.github.com>
2024-10-26 11:32:19 -0600

    Added geo location

--- timesheet_app/lib/timesheet_home_page.dart
+++ timesheet_app/lib/timesheet_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class TimesheetHomePage extends StatefulWidget {
   @override
@@ -9,17 +10,57 @@ class _TimesheetHomePageState extends State<TimesheetHomePage> {
   DateTime? _startTime;
   DateTime? _endTime;
   Duration? _elapsedTime;
-  List<Map<String, DateTime>> _timestamps = [];
+  List<Map<String, dynamic>> _timestamps = [];
 
-  void _logTime() {
+  @override
+  void initState() {
+    super.initState();
+    _requestLocationPermission();
+  }
+
+  Future<void> _requestLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+  }
+
+  Future<Position> _getCurrentLocation() async {
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
+
+  void _logTime() async {
+    Position position = await _getCurrentLocation();
     setState(() {
       if (_startTime == null) {
         _startTime = DateTime.now();
-        _timestamps.add({'start': _startTime!});
+        _timestamps.add({
+          'start': _startTime!,
+          'startLocation': position,
+        });
       } else {
         _endTime = DateTime.now();
         _elapsedTime = _endTime!.difference(_startTime!);
         _timestamps.last['end'] = _endTime!;
+        _timestamps.last['endLocation'] = position;
         _startTime = null; // Reset for next use
       }
     });
@@ -28,8 +69,8 @@ class _TimesheetHomePageState extends State<TimesheetHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
-        title: Text('Timesheet App 2'),
+      appBar: AppBar(
+        title: Text('Timesheet App'),
       ),
       body: Center(
         child: Column(
@@ -37,31 +78,83 @@ class _TimesheetHomePageState extends State<TimesheetHomePage> {
           children: <Widget>[
             ElevatedButton(
               onPressed: _logTime,
-              child: Text(_startTime == null ? 'Start Work 2' : 'Stop Work'),
+              child: Text(_startTime == null ? 'Start Work' : 'Stop Work'),
             ),
             if (_timestamps.isNotEmpty)
               Expanded(
                 child: ListView.builder(
                   itemCount: _timestamps.length,
                   itemBuilder: (context, index) {
-                    final timestamp = _timestamps[index];
-                    return ListTile(
-                      title: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Start: ${timestamp['start']}',
-                            textAlign: TextAlign.center,
-                          ),
-                          if (timestamp.containsKey('end'))
-                            Text(
-                              'End: ${timestamp['end']}',
-                              textAlign: TextAlign.center,
-                            ),
-                        ],
-                      ),
-                    );
-                  },
+  final timestamp = _timestamps[index];
+  return ListTile(
+    title: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text.rich(
+          TextSpan(
+            children: [
+              TextSpan(
+                text: 'Start: ',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              TextSpan(
+                text: '${timestamp['start']}',
+              ),
+            ],
+          ),
+          textAlign: TextAlign.center,
+        ),
+        Text.rich(
+          TextSpan(
+            children: [
+              TextSpan(
+                text: 'Start Location: ',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              TextSpan(
+                text: '${timestamp['startLocation'].latitude}, ${timestamp['startLocation'].longitude}',
+              ),
+            ],
+          ),
+          textAlign: TextAlign.center,
+        ),
+        if (timestamp.containsKey('end'))
+          Column(
+            children: [
+              Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'End: ',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text: '${timestamp['end']}',
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'End Location: ',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text: '${timestamp['endLocation'].latitude}, ${timestamp['endLocation'].longitude}',
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+      ],
+    ),
+  );
+},
                 ),
               ),
           ],
